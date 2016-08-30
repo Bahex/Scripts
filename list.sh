a9be064cc740077faf8cc0308de01207bb1a9725
Bahex <Bahex@users.noreply.github.com>
2016-08-30 17:31:16 +0300

    Script for listing lines from a file with a predetermined prefix

---
+++ list.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+[[ $# -eq 1 && -f "$1" && -r "$1" ]] || { echo "This script needs a file that is READABLE and PLAINTEXT."; exit 1; }
+
+export i=1
+cat $1 | while read line; do
+    echo "$i →  ${line}"
+    i=$((i+1))
+done
