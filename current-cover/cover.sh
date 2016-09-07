6c4172775d9c0bcfcfcb1fe4c699f407f6d497b8
Bahex <Bahex@users.noreply.github.com>
2016-09-07 16:15:17 +0300

    working

--- current-cover/cover.sh
+++ current-cover/cover.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-sdir=`realpath $0`
-sdir=${sdir%/*}
-
+### USER CONFIG
 MPD_MUSIC_PATH=$HOME/Music
 TMP_COVER_PATH="/tmp/mpd-track-cover"
 
+#Get script path
+sdir=`realpath $0`
+sdir=${sdir%/*}
+
+#Find images in album's directory
 albumart_in_dir(){
     echo "${MPD_CURRENT%/*}"/`ls "${MPD_CURRENT%/*}" | grep "album.*" | head -n 1`
 }
 
+#Try to find an album cover. If there is no suitable image, fallback on "No Cover"
 ext_cvr(){
     MPD_CURRENT="$MPD_MUSIC_PATH/`mpc --format '%file%' current`"
 
@@ -22,4 +26,5 @@ ext_cvr(){
     fi
 }
 
-mpc idleloop | while read i; do ext_cvr; done
+#mpc idleloop | while read i; do ext_cvr; done
+while mpc idle; do ext_cvr; done
