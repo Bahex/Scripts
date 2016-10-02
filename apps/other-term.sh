3041b19e2636455a3bd2d3371fa95b689bce0315
Bahex <Bahex@users.noreply.github.com>
2016-10-02 15:44:17 +0300

    Multiple terminal styles

--- apps/other-term.sh
+++ apps/other-term.sh
@@ -1,1 +1,18 @@
-urxvt -depth 32 -bg rgba:0000/0000/0000/cccc &
+#!/bin/bash
+
+STYLE="$(echo -e "True\nHalf\nNormal" | rofi -dmenu -p "Term: ")"
+
+case $STYLE in
+	True)
+		urxvt -depth 32 -bg rgba:0000/0000/0000/0000 &
+		;;
+	Half)
+		urxvt -depth 32 -bg rgba:0000/0000/0000/cccc &
+		;;
+	Normal)
+		urxvt &
+		;;
+	*)
+		echo "Fuck"
+		;;
+esac
